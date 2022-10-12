a0ac115d962b4e16875734d16c166cdf2e449a61
yangxu351 <xuyang_ustb@163.com>
2022-10-12 04:23:27 -0400

    adjust for xilin wdt

--- lib/datasets/config_dataset.py
+++ lib/datasets/config_dataset.py
@@ -30,12 +30,19 @@ __D.CITYSCAPE_CAR = "/VOC2007"
 __D.CITYSCAPE = "VOC2007"
 __D.FOGGYCITY = "VOC2007"
 
+# __D.BASE_DATA_DIR = "/data/users/yang/data"
+# __D.DEV_DATA_DIR = "data/real_syn_nwpu_vockit"
+# #Tag:
+# __D.DATASET = 'SYN_NWPU_C1'
+# __D.DATABASE = 'syn_nwpu_bkg_shdw_rndsolar_sizefactor1_multimodels_negtrn_fixsigma_C1_v6'
+# __D.DATASET_T = 'REAL_NWPU_C1'
+
 __D.BASE_DATA_DIR = "/data/users/yang/data"
-__D.DEV_DATA_DIR = "data/real_syn_nwpu_vockit"
+__D.DEV_DATA_DIR = "data/real_syn_wdt_vockit"
 #Tag:
-__D.DATASET = 'SYN_NWPU_C1'
-__D.DATABASE = 'syn_nwpu_bkg_shdw_rndsolar_sizefactor1_multimodels_negtrn_fixsigma_C1_v6'
-__D.DATASET_T = 'REAL_NWPU_C1'
+__D.DATASET = 'synthetic_data_wdt'
+__D.DATABASE = 'syn_wdt_rnd_sky_rnd_solar_rnd_cam_p3_shdw_step40'
+__D.DATASET_T = 'xilin_wdt'
 
 def _merge_a_into_b(a, b):
   """Merge config dictionary a into config dictionary b, clobbering the
