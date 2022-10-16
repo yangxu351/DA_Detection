8221a6394bbe67465f83f6ec49b353771f5cc937
yangxu351 <xuyang_ustb@163.com>
2022-10-16 09:27:09 -0400

    bs=1

--- train_scripts/syn2real.sh
+++ train_scripts/syn2real.sh
@@ -4,4 +4,11 @@
 # export LD_LIBRARY_PATH=/home/lab/yangDir/cuda-9.0/lib64:$LD_LIBRARY_PATH
 
 
-python trainval_net_global_local.py --cuda --net res50 --dataset SYN_NWPU_C1 --dataset_t REAL_NWPU_C1 --gc --lc 
+# python trainval_net_global_local.py --cuda --net res50 --dataset SYN_NWPU_C1 --dataset_t REAL_NWPU_C1 --gc --lc 
+
+
+python trainval_net_global_local.py --cuda --device cuda:0 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --bs 1
+
+# python trainval_net_global_local_batch.py --cuda --device cuda:2 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --bs 4
+
+# python trainval_net_global_local_batch.py --cuda --device cuda:2 --net res50 --dataset SYN_NWPU_C1 --dataset_t REAL_NWPU_C1 --gc --lc --bs 4
