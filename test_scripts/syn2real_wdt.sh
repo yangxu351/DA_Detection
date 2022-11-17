560bcf3d883420bebbf49fee115e2eba0efbb8dd
yangxu351 <xuyang_ustb@163.com>
2022-11-17 07:01:28 -0500

    add new data seeds for training and testing

--- test_scripts/syn2real_wdt.sh
+++ test_scripts/syn2real_wdt.sh
@@ -1,4 +1,53 @@
 #!/bin/sh
 # CUDA_VISIBLE_DEVICES=$1 python test_net_global_local.py --cuda --net vgg16 --dataset cityscape_car --gc --lc --load_name $2
 
-python test_net_global_local.py --cuda --net res50 --dataset_test xilin_wdt --gc --lc --load_name net_res50_target_xilin_wdt_lr0.0001_eta_0.1_lcTrue_glTrue_gamma_5_session_1_epoch_50.pth
+# python test_net_global_local.py --cuda --net res50 --database_test xilin_wdt --gc --lc --load_name net_res50_target_xilin_wdt_lr0.0001_eta_0.1_lcTrue_glTrue_gamma_5_session_1_epoch_20.pth
+
+# python test_net_global_local.py --cuda --net res50 --database_test xilin_wdt --gc --lc --load_name 20221016_2203/target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_glTrue_gamma_5_session_1_epoch_20_noflip.pth
+
+# python test_net_local.py --cuda --net res50 --database_test xilin_wdt --gc --lc --load_name target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_glTrue_gamma_5_session_1_epoch_30_flipFalse.pth
+
+# python test_net_global_local.py --cuda --net res50 --database_test xilin_wdt --gc --lc --load_name  net_res50_target_xilin_wdt_lr0.0001_eta_0.1_lcTrue_glTrue_gamma_5_session_1_epoch_50.pth
+
+''' global + local '''
+# python test_net_global_local.py --cuda --net res101 --database_test xilin_wdt --gc --lc --load_name 20221020_0107/target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1090
+# python test_net_global_local.py --cuda --net res101 --database_test xilin_wdt --gc --lc --load_name 20221021_0602/target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.094
+# python test_net_global_local.py --cuda --net res101 --database_test xilin_wdt --gc --lc --load_name 20221021_0602/target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gcTrue_gamma_5_session_1_epoch_20_flipFalse.pth # 0.0939
+
+''' global + local  for aug val'''
+# python test_net_global_local.py --cuda --net res101 --database_test xilin_wdt --aug --gc --lc --load_name 20221020_0107/target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gcTrue_gamma_5_session_1_epoch_20_flipFalse.pth # 0.103
+# python test_net_global_local.py --cuda --net res101 --database_test xilin_wdt --aug --gc --lc --load_name 20221020_0107/target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gcTrue_gamma_5_session_1_epoch_10_flipFalse.pth # 0.105
+#####----------change optimizer scheduler
+#  python test_net_global_local.py --cuda --net res50 --database_test xilin_wdt --aug --gc --lc --load_name 20221112_0907/target_xilin_wdt_lr0.0001_bs1_eta_0.1_lcTrue_gcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1114
+
+''' local '''
+# python test_net_local.py --cuda --net res101 --database_test xilin_wdt --lc --load_name 20221020_0109/local_target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1737
+# python test_net_local.py --cuda --net res101 --database_test xilin_wdt --lc --load_name 20221021_0605/local_target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1609
+# python test_net_local.py --cuda --net res101 --database_test xilin_wdt --lc --load_name 20221021_0605/local_target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_20_flipFalse.pth # 0.1633
+# python test_net_local.py --cuda --net res101 --database_test xilin_wdt --lc --load_name 20221021_0605/local_target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1609
+
+'''local  for aug val'''
+# python test_net_local.py --cuda --net res101 --database_test xilin_wdt --aug --lc --load_name 20221020_0109/local_target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_20_flipFalse.pth # 0.108
+# python test_net_local.py --cuda --net res101 --database_test xilin_wdt --aug --lc --load_name 20221020_0109/local_target_xilin_wdt_lr0.001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_10_flipFalse.pth # 0.116
+#####----------change optimizer scheduler
+# python test_net_local.py --cuda --net res50 --database_test xilin_wdt --aug --lc --load_name 20221112_0935/local_target_xilin_wdt_lr0.0001_bs1_eta_0.1_lcTrue_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1165
+
+''' global '''
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --gc --load_name 20221020_0110/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_30_flipFalse.pth
+
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --gc --load_name 20221021_0606/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_20_flipFalse.pth # 0.1910
+
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --gc --load_name 20221021_0606/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_30_flipFalse.pth # 0.1930
+
+'''global for aug val dataseed-1'''
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --aug --gc --load_name 20221021_0606/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_20_flipFalse.pth # 0.115
+
+python test_net_global.py --cuda --net res101 --database_test xilin_wdt --aug --gc --load_name 20221021_0606/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_10_flipFalse.pth # 0.132
+
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --aug --gc --load_name 20221021_0606/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_30_flipFalse.pth # 0.110
+
+'''global for aug val dataseed1'''
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --aug --gc --load_name 20221116_1219_data_seed1/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_20_flipFalse.pth  # 0.0051
+
+'''global for aug val dataseed2'''
+# python test_net_global.py --cuda --net res101 --database_test xilin_wdt --aug --gc --load_name 20221116_1221_data_seed2/global_target_xilin_wdt_eta_0.1_efocal_False_gc_True_gamma_5_session_1_epoch_30_flipFalse.pth
--- train_scripts/syn2real.sh
+++ train_scripts/syn2real.sh
@@ -3,12 +3,39 @@
 # export PATH=/home/lab/yangDir/cuda-9.0/bin:$PATH
 # export LD_LIBRARY_PATH=/home/lab/yangDir/cuda-9.0/lib64:$LD_LIBRARY_PATH
 
-
+# CUDA_VISIBLE_DEVICES=3
 # python trainval_net_global_local.py --cuda --net res50 --dataset SYN_NWPU_C1 --dataset_t REAL_NWPU_C1 --gc --lc 
 
 
-python trainval_net_global_local.py --cuda --device cuda:0 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --bs 1
+# python trainval_net_global_local.py --cuda --device cuda:0 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --bs 1 
 
-# python trainval_net_global_local_batch.py --cuda --device cuda:2 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --bs 4
+# python trainval_net_global_local.py --cuda --device cuda:0 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --use_tfb --bs 1 --epochs 20 --lr 1e-3
+
+########## global + local
+# python trainval_net_global_local.py --cuda --device 3 --net res101 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --use_tfb --bs 1 --epochs 30 --lr 1e-3
+
+########### local
+# python trainval_net_local.py --cuda --device 2 --net res101 --dataset synthetic_data_wdt --dataset_t xilin_wdt --lc  --use_tfb --bs 1 --epochs 30 --lr 1e-3
+
+########### global 
+#---------------dataseed17
+# python trainval_net_global.py --cuda --device 1 --net res101 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --use_tfb --bs 1 --epochs 30 --lr 1e-3
+#---------------dataseed1
+# python trainval_net_global.py --cuda --device 3 --net res101 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --use_tfb --bs 1 --epochs 30 --lr 1e-3
+
+#---------------dataseed2
+# python trainval_net_global.py --cuda --device 1 --net res101 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --use_tfb --bs 1 --epochs 30 --lr 1e-3
+
+# python trainval_net_global_local_batch.py --cuda --device cuda:1 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --bs 4
 
 # python trainval_net_global_local_batch.py --cuda --device cuda:2 --net res50 --dataset SYN_NWPU_C1 --dataset_t REAL_NWPU_C1 --gc --lc --bs 4
+
+
+####----------global+local-----opt------------------------------############
+# python trainval_net_global_local_opt.py --cuda --device 3 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc --lc --use_tfb --bs 1 --epochs 30 
+
+####----------global-----opt ------------------------------############
+# python trainval_net_global_opt.py --cuda --device 2 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --gc  --use_tfb --bs 1 --epochs 30 
+
+####----------local-----opt ------------------------------############
+# python trainval_net_local_opt.py --cuda --device 1 --net res50 --dataset synthetic_data_wdt --dataset_t xilin_wdt --lc  --use_tfb --bs 1 --epochs 30 
